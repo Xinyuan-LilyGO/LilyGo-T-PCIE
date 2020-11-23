cf4afa08bdcc8bd71e780df24f2993fe92e8c6b0
lewisxhe <lewisxhe@outlook.com>
2020-11-23 08:51:08 +0800

    Added ATDebug example

---
+++ examples/ATDebug/ATDebug.ino
@@ -0,0 +1,128 @@
+// Select your modem:
+// #define TINY_GSM_MODEM_SIM868
+// #define TINY_GSM_MODEM_SIM7000  
+// #define TINY_GSM_MODEM_SIM7600
+
+// Set serial for debug console (to the Serial Monitor, default speed 115200)
+#define SerialMon Serial
+
+// Set serial for AT commands (to the module)
+// Use Hardware Serial on Mega, Leonardo, Micro
+#define SerialAT Serial1
+
+// See all AT commands, if wanted
+// #define DUMP_AT_COMMANDS
+
+// Define the serial console for debug prints, if needed
+#define TINY_GSM_DEBUG SerialMon
+
+// Range to attempt to autobaud
+#define GSM_AUTOBAUD_MIN 9600
+#define GSM_AUTOBAUD_MAX 57600
+
+#include <TinyGsmClient.h>
+#ifdef DUMP_AT_COMMANDS
+#include <StreamDebugger.h>
+StreamDebugger debugger(SerialAT, SerialMon);
+TinyGsm modem(debugger);
+#else
+TinyGsm modem(SerialAT);
+#endif
+#define uS_TO_S_FACTOR          1000000ULL  /* Conversion factor for micro seconds to seconds */
+#define TIME_TO_SLEEP           60          /* Time ESP32 will go to sleep (in seconds) */
+
+#define PIN_TX                  27
+#define PIN_RX                  26
+#define UART_BAUD               115200
+#define PWR_PIN                 4
+#define LED_PIN                 12
+#define POWER_PIN               25
+
+bool reply = false;
+
+void modem_on()
+{
+    // Set-up modem  power pin
+    Serial.println("\nStarting Up Modem...");
+    pinMode(PWR_PIN, OUTPUT);
+    digitalWrite(PWR_PIN, HIGH);
+    delay(300);
+    digitalWrite(PWR_PIN, LOW);
+    delay(10000);
+
+    int i = 10;
+    Serial.println("\nTesting Modem Response...\n");
+    Serial.println("****");
+    while (i) {
+        SerialAT.println("AT");
+        delay(500);
+        if (SerialAT.available()) {
+            String r = SerialAT.readString();
+            Serial.println(r);
+            if ( r.indexOf("OK") >= 0 ) {
+                reply = true;
+                break;;
+            }
+        }
+        delay(500);
+        i--;
+    }
+    Serial.println("****\n");
+}
+
+void setup()
+{
+    // Set console baud rate
+    SerialMon.begin(115200);
+    delay(10);
+
+    // Onboard LED light, it can be used freely
+    pinMode(LED_PIN, OUTPUT);
+    digitalWrite(LED_PIN, LOW);
+
+    // POWER_PIN : This pin controls the power supply of the SIM7600
+    pinMode(POWER_PIN, OUTPUT);
+    digitalWrite(POWER_PIN, HIGH);
+
+    // PWR_PIN ： This Pin is the PWR-KEY of the SIM7600
+    // The time of active low level impulse of PWRKEY pin to power on module , type 500 ms
+    pinMode(PWR_PIN, OUTPUT);
+    digitalWrite(PWR_PIN, HIGH);
+    delay(500);
+    digitalWrite(PWR_PIN, LOW);
+
+    delay(1000);
+
+    SerialAT.begin(UART_BAUD, SERIAL_8N1, PIN_RX, PIN_TX);
+
+    DBG("Wait...");
+    int retry = 5;
+    while (!reply && retry--){
+        modem_on();
+    }
+
+    if (reply) {
+        Serial.println(F("***********************************************************"));
+        Serial.println(F(" You can now send AT commands"));
+        Serial.println(F(" Enter \"AT\" (without quotes), and you should see \"OK\""));
+        Serial.println(F(" If it doesn't work, select \"Both NL & CR\" in Serial Monitor"));
+        Serial.println(F(" DISCLAIMER: Entering AT commands without knowing what they do"));
+        Serial.println(F(" can have undesired consiquinces..."));
+        Serial.println(F("***********************************************************\n"));
+    } else {
+        Serial.println(F("***********************************************************"));
+        Serial.println(F(" Failed to connect to the modem! Check the baud and try again."));
+        Serial.println(F("***********************************************************\n"));
+    }
+
+}
+
+void loop()
+{
+    while (SerialAT.available()) {
+        Serial.write(SerialAT.read());
+    }
+    while (Serial.available()) {
+        SerialAT.write(Serial.read());
+    }
+}
